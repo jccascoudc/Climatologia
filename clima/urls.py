3bc0137556af795e33e5ac433b98995d63ecc0a5
jccascoudc <jccasco@udc.edu.ar>
2024-09-13 18:05:59 -0300

    Se agrega funcionalidad para toma de datos y grafica en tiempo real

--- clima/urls.py
+++ clima/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('climatologia/', views.vista_climatologica, name='vista_climatologica'),
+    path('iniciarlectura/', views.iniciar_lectura, name='iniciar_lectura_arduino'),
+    path('detenerlectura/', views.detener_lectura, name='detener_lectura_arduino'),
+    path('api/datos-climatologicos/', views.datos_climatologicos_api, name='datos_climatologicos_api'),
 ]
